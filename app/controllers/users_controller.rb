bfc4aa5a89c225803e34c9a0cc80eb39b840335d
taketakedesu <ketaketadesu@gmail.com>
2020-10-19 03:51:39 +0900

    error changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
-require 'test_helper'
+class UsersController < ApplicationController
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
-
-  test "should get new" do
-    get signup_path
-    assert_response :success
+  def new
   end
 end
